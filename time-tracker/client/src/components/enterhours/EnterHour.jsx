d4e270299b518beaffe5d6d173baafc912138c0a
Mia-Y-Kelly <miayn24@gmail.com>
2023-04-10 14:11:06 -0400

    Create wireframe for lab time entry page

--- time-tracker/client/src/components/enterhours/EnterHour.jsx
+++ time-tracker/client/src/components/enterhours/EnterHour.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import "./enterhour.css"
 
 function EnterHour() {
   return (
-    <div>EnterHour</div>
+    <div className='form-container'>
+      <form>
+        <h2>Enter Lab Time</h2>
+        <input type="date"/>
+        <select>
+          <option disabled> Select Team</option>
+            <option>Systems</option>
+            <option>Networking</option>
+            <option>Security</option>
+            <option>Dev</option>
+        </select>
+        <input type='number' min=".5" max="18" step={.5} placeholder='Hours...'/>
+        <textarea name="text" maxlength={50} wrap='soft'/>
+        <input type='submit' value="Submit"/>
+      </form>
+    </div>
   )
 }
 
