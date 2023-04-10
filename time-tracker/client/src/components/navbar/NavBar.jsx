82c713f4c920dfb2c06790647b455348d4b4c5d7
Mia-Y-Kelly <miayn24@gmail.com>
2023-04-10 17:18:56 -0400

    Added dynamic functionality for navbar

--- time-tracker/client/src/components/navbar/NavBar.jsx
+++ time-tracker/client/src/components/navbar/NavBar.jsx
@@ -9,33 +9,47 @@ import OfficerPortal from '../officerportal/OfficerPortal'
 import ViewHour from '../viewhours/ViewHour'
 
 const NavBar = () => {
-    const [activeNav, setActiveNav] = useState('#');
+    const [hamburgerState, setHamburgerState] = useState('hamburger_passive');
+    const [navState, setNavState] = useState("navbar_passive");
+    
+    function showNav() {
+        if(hamburgerState === "hamburger_passive") {
+            setHamburgerState('hamburger_active');
+            setNavState("navbar_active");
+        }
+        else {
+            setHamburgerState('hamburger_passive');
+            setNavState("navbar_passive");
+        }
+                    
+    }
+    
     return (
             <section id='nav'>
-                <div className="container navbar__container">
+                <div className="navbar__container">
                     <div className="navbar__icon">
                         <img src={logo} className="navbar__image" alt="logo" />
                     </div>
                     <h2>NCSA Lab Hours Manager</h2>
-                    <nav>
+                
+                    <nav className={navState}> 
+                        <a>
+                            <Link to="/" className={hamburgerState} onClick={showNav}>  <GiHamburgerMenu /> </Link>
+                        </a>
                         <ul>
                             <a>
-                                <Link to="/enterhour"> Enter Hours</Link>
+                                <Link to="/enterhour" className='links'> Enter Hours </Link>
                             </a>
                             <a>
-                                <Link to="/viewhour">  View Hours </Link>
+                                <Link to="/viewhour" className='links'> View Hours </Link>
                             </a>
                             <a>
-                                <Link to="/officerportal">  Officer Portal </Link>
+                                <Link to="/officerportal" className='links'> Officer Portal </Link>
                             </a>
                             <a>
-                                <Link to="/">  Sign Out </Link>
-                            </a>
-                            <a className='hamburger__icon'>
-                                <Link to="/" className='hamburger__icon'>  <GiHamburgerMenu /> </Link>
+                                <Link to="/" className='links'>  Sign Out </Link>
                             </a>
                         </ul>
-                        
                     </nav>
                 </div>
                 <hr className="accent"/>
